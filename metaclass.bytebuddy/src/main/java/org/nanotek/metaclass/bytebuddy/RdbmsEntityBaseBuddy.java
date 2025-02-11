3b291b3fa3380bf1c42863dfe3b6e8bb4ffb5a1e
Jose Carlos Canova <jose.c.canova@gmail.com>
2025-02-11 09:05:03 -0300

    first interaction of
    https://github.com/users/JoseCanova/projects/3?pane=issue&itemId=97280382

--- metaclass.bytebuddy/src/main/java/org/nanotek/metaclass/bytebuddy/RdbmsEntityBaseBuddy.java
+++ metaclass.bytebuddy/src/main/java/org/nanotek/metaclass/bytebuddy/RdbmsEntityBaseBuddy.java
@@ -1,12 +1,10 @@
 package org.nanotek.metaclass.bytebuddy;
 
-import java.util.List;
+import java.util.Optional;
 
 import org.nanotek.meta.model.rdbms.RdbmsMetaClass;
-import org.nanotek.meta.model.rdbms.RdbmsMetaClassAttribute;
-import org.nanotek.metaclass.bytebuddy.annotations.AnnotationDescriptionFactory;
+import org.nanotek.metaclass.bytebuddy.attributes.AttributeBaseBuilder;
 
-import net.bytebuddy.description.annotation.AnnotationDescription;
 import net.bytebuddy.dynamic.DynamicType.Builder;
 
 public class RdbmsEntityBaseBuddy 
@@ -23,40 +21,18 @@ implements EntityBaseByteBuddy {
 	}
 
 	public Class<?> getLoadedClassInDeaultClassLoader(){
-		Builder<?> builder =  this.generateBuilderWithClassName
-				(this.generateByteBuddy() , metaClass);
-
-			Holder<Builder<?>> holder = Holder.of(builder);
-			List<RdbmsMetaClassAttribute> atts = metaClass.getMetaAttributes();
-			atts
-			.stream()
-			.forEach(att -> {
-				AnnotationDescription[] descs = buildAnnotations(att);
-								Class<?> theJavaClass = getJavaClass(att.getClazz());
-								Builder<?> theBuilder = holder.get().orElseThrow();
-								holder.put(theBuilder
-										.defineProperty(att.getFieldName(), theJavaClass)
-										.annotateType(descs));
-			});
+		
+		Builder<?> builder =   AttributeBaseBuilder
+				.on()
+				.generateClassAttributes(metaClass , 
+				this.generateBuilderWithClassName
+				(this.generateByteBuddy() , metaClass));
 			Class<?> loaded = 
-			holder.get()
+			Optional.of(builder)
 			.map(b -> b.make().load(getClass().getClassLoader())
 					.getLoaded()) .orElseThrow() ;
 			
 			return loaded;
-	}
-	
-	@SuppressWarnings("rawtypes")
-	AnnotationDescription[] buildAnnotations(RdbmsMetaClassAttribute att) {
-		return new AnnotationDescriptionFactory.AttributeAnnotationDescriptionBuilder().build(att);
-	}
-	
-	private Class<?> getJavaClass(String clazz) {
-		try { 
-			return Class.forName(clazz);
-		}catch(Exception ex) {
-			throw new RuntimeException(ex);
-		}
 	}
 	
 	public RdbmsMetaClass getMetaClass() {
---
+++ metaclass.bytebuddy/src/main/java/org/nanotek/metaclass/bytebuddy/attributes/AttributeBaseBuilder.java
@@ -0,0 +1,56 @@
+package org.nanotek.metaclass.bytebuddy.attributes;
+
+
+import java.util.List;
+
+import org.nanotek.meta.model.rdbms.RdbmsMetaClass;
+import org.nanotek.meta.model.rdbms.RdbmsMetaClassAttribute;
+import org.nanotek.metaclass.bytebuddy.Holder;
+import org.nanotek.metaclass.bytebuddy.annotations.AnnotationDescriptionFactory;
+
+import net.bytebuddy.description.annotation.AnnotationDescription;
+import net.bytebuddy.dynamic.DynamicType.Builder;
+
+
+//TODO: verify best is class name is the more appropriate name.
+public interface AttributeBaseBuilder<T extends Builder<?> , M extends RdbmsMetaClass> {
+
+	
+	public static <T extends Builder<?> , M extends RdbmsMetaClass>  
+		AttributeBaseBuilder<T,M> on(){
+		return new AttributeBaseBuilder<T,M>(){};
+	}
+	
+	
+	//TODO: Fix type check 
+	//TODO: verify method implementation for a functional method
+	@SuppressWarnings("unchecked")
+	default T generateClassAttributes(M metaClass , T builder) {
+		Holder<Builder<?>> holder = Holder.of(builder);
+		List<RdbmsMetaClassAttribute> atts = metaClass.getMetaAttributes();
+		atts
+		.stream()
+		.forEach(att -> {
+			AnnotationDescription[] descs = buildAnnotations(att);
+							Class<?> theJavaClass = getJavaClass(att.getClazz());
+							Builder<?> theBuilder = holder.get().orElseThrow();
+							holder.put(theBuilder
+									.defineProperty(att.getFieldName(), theJavaClass)
+									.annotateType(descs));
+		});
+		
+		return (T)holder.get(). orElseThrow();
+	}
+	
+	default <K extends RdbmsMetaClassAttribute> AnnotationDescription[] buildAnnotations(K att) {
+		return new AnnotationDescriptionFactory.AttributeAnnotationDescriptionBuilder<K>().build(att);
+	}
+	
+	default Class<?> getJavaClass(String clazz) {
+		try { 
+			return Class.forName(clazz);
+		}catch(Exception ex) {
+			throw new RuntimeException(ex);
+		}
+	}
+}
