49523b2b08e38945bbd631ee599d25196f5b2980
Jose Carlos Canova <jose.c.canova@gmail.com>
2025-04-09 10:58:59 -0300

    Created a registry or processed fks
    to be used during metaclass processing to "facilitate"

---
+++ metaclass.bytebuddy/src/main/java/org/nanotek/metaclass/ProcessedForeignKeyRegistry.java
@@ -0,0 +1,39 @@
+package org.nanotek.metaclass;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+
+import org.nanotek.meta.model.rdbms.RdbmsMetaClass;
+import org.nanotek.meta.model.rdbms.RdbmsMetaClassForeignKey;
+
+public class ProcessedForeignKeyRegistry {
+
+	Map<RdbmsMetaClassForeignKey,RdbmsMetaClass> registry;
+	
+	public ProcessedForeignKeyRegistry() {
+		postConstruct();
+	}
+
+	private void postConstruct(){
+		registry = new HashMap<>();
+	}
+	
+	public RdbmsMetaClassForeignKey registryForeignKeyMetaClass
+	  (RdbmsMetaClassForeignKey fk , RdbmsMetaClass mc ) {
+		registry.put(fk, mc);
+		return fk;
+	}
+	
+	public Set<RdbmsMetaClassForeignKey> getProcessedForeignKeys() {
+		return registry.keySet();
+	}
+	
+	//TODO: 
+ 	public RdbmsMetaClass getMetaClassForeignKey(RdbmsMetaClassForeignKey key) {
+		return Optional
+				.ofNullable(registry.get(key))
+				.orElseThrow();
+	}
+}
