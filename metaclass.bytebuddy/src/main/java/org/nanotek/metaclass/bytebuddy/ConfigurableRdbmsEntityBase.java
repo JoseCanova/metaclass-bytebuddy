67adacddb2033f2b8d544f2ac17427bf35c7015f
Jose Carlos Canova <jose.c.canova@gmail.com>
2025-04-30 14:11:11 -0300

    Change design of class to comply with a configurable strategy provider.

--- metaclass.bytebuddy/src/main/java/org/nanotek/metaclass/bytebuddy/ConfigurableRdbmsEntityBase.java
+++ metaclass.bytebuddy/src/main/java/org/nanotek/metaclass/bytebuddy/ConfigurableRdbmsEntityBase.java
@@ -2,43 +2,22 @@ package org.nanotek.metaclass.bytebuddy;
 
 import org.nanotek.meta.model.rdbms.RdbmsMetaClass;
 
-public class ConfigurableRdbmsEntityBase  {
+public class ConfigurableRdbmsEntityBase {
 
-	private boolean annotatedValidation = false;
-	
-	private boolean annotateJpa = false;
-	
-	private boolean annotateSpring = false;
+	private String strategy;
 	
 	
-	public ConfigurableRdbmsEntityBase(RdbmsMetaClass metaClass) {
+	public ConfigurableRdbmsEntityBase(String arg) {
 		super();
-	}
-	
-	public boolean isAnnotatedValidation() {
-		return annotatedValidation;
+		strategy = arg;
 	}
 
-	public void setAnnotatedValidation(boolean annotatedValidation) {
-		this.annotatedValidation = annotatedValidation;
+	public EntityBaseByteBuddy getEntityBaseByteBuddy(RdbmsMetaClass metaClass) {
+		switch (strategy) {
+			default:
+				return new RdbmsEntityBaseBuddy(metaClass);
+		}
+			
 	}
-
-	public boolean isAnnotateJpa() {
-		return annotateJpa;
-	}
-
-	public void setAnnotateJpa(boolean annotateJpa) {
-		this.annotateJpa = annotateJpa;
-	}
-
-	public boolean isAnnotateSpring() {
-		return annotateSpring;
-	}
-
-	public void setAnnotateSpring(boolean annotateSpring) {
-		this.annotateSpring = annotateSpring;
-	}
-	
-	
 
 }
