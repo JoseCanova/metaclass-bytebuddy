b216498dd7f0fa7371caad6c7167729fa43448ea
Jose Carlos Canova <jose.c.canova@gmail.com>
2025-05-09 12:22:34 -0300

    Added description for the main methods of the AnnotationDescription Factory.

--- metaclass.bytebuddy/src/main/java/org/nanotek/metaclass/bytebuddy/annotations/AnnotationDescriptionFactory.java
+++ metaclass.bytebuddy/src/main/java/org/nanotek/metaclass/bytebuddy/annotations/AnnotationDescriptionFactory.java
@@ -31,6 +31,7 @@ import net.bytebuddy.description.annotation.AnnotationDescription;
  * Class that define base methods to annotations that will added on a Type.
  * Initially it provides support for Class level and Attribute level annotations, 
  * since the Component by it self is specific to JPA Entity Definitions.
+ * The empty methods are intended to be implemented by the Derived Classes.
  *
  * @param <T> - the Kind of annotations
  * @param <K> - The Type that provide the elements to build the AnnotationDescription.
@@ -38,18 +39,36 @@ import net.bytebuddy.description.annotation.AnnotationDescription;
 public interface AnnotationDescriptionFactory
 <T extends Annotation , K> {
 
+	/**
+	 * Empty method that build an AnnotationDescription.
+	 * @return
+	 */
 	default Optional<AnnotationDescription>  buildAnnotationDescription(){
 		return Optional.empty();
 	}
 	
+	/**
+	 * Build an ByteBuddy AnnotationDescription of a given type T
+	 * @param annotationType
+	 * @return
+	 */
 	default AnnotationDescription buildAnnotationDescription(Class<T> annotationType) {
 		return AnnotationDescription.Builder.ofType(annotationType).build();
 	}
+	
 	
 	default Optional<AnnotationDescription> buildAnnotationDescription(K ma) {
 		return Optional.empty();
 	}
 	
+	/**
+	 * Given a representation of a ForeignKey find in the Representation of the Table which attribute 
+	 * is the orign of the foreign key.
+	 * 
+	 * @param fk - representation of the ForeignKey in the Rdbms.
+	 * @param metaClass - Representation of the Table in the Rdbms.
+	 * @return The Attribute which is the orign of the ForeignKey Relation.
+	 */
 	default  RdbmsMetaClassAttribute findIdAttribute(RdbmsMetaClassForeignKey fk , RdbmsMetaClass metaClass) {
 		return metaClass
 				.getMetaAttributes()
@@ -81,9 +100,20 @@ public interface AnnotationDescriptionFactory
 		throw new RuntimeException("This method is deprecated since the processes fk registry was added");
 	}
 	
-	default Optional<AnnotationDescription> buildForeignAnnotationDescription
-			(RdbmsMetaClassAttribute ma, RdbmsMetaClass metaClass,BuilderMetaClassRegistry builderMetaClassRegistry,
-			ProcessedForeignKeyRegistry processedForeignKeyRegistry) {	
+	/**
+	 * @deprecated
+	 * @param metaClassAttribute
+	 * @param metaClass
+	 * @param builderMetaClassRegistry
+	 * @param processedForeignKeyRegistry
+	 * @return
+	 */
+	default Optional<AnnotationDescription> 
+	buildForeignAnnotationDescription
+			(RdbmsMetaClassAttribute metaClassAttribute , 
+					RdbmsMetaClass metaClass,
+					BuilderMetaClassRegistry builderMetaClassRegistry,
+					ProcessedForeignKeyRegistry processedForeignKeyRegistry) {	
 			return Optional.empty();
 	}
 	
