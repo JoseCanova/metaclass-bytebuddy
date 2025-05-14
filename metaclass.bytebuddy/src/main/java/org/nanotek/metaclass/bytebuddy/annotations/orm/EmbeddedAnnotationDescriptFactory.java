b4f747a6f8263b783f25db38ffa7ec41a27af928
Jose Carlos Canova <jose.c.canova@gmail.com>
2025-05-14 12:02:19 -0300

    Added EmbeddedIdA..D..Factory

---
+++ metaclass.bytebuddy/src/main/java/org/nanotek/metaclass/bytebuddy/annotations/orm/EmbeddedAnnotationDescriptFactory.java
@@ -0,0 +1,11 @@
+package org.nanotek.metaclass.bytebuddy.annotations.orm;
+
+import org.nanotek.meta.model.rdbms.RdbmsMetaClass;
+import org.nanotek.metaclass.bytebuddy.annotations.AnnotationDescriptionFactory;
+
+import jakarta.persistence.Entity;
+
+public class EmbeddedAnnotationDescriptFactory
+implements AnnotationDescriptionFactory<Entity, RdbmsMetaClass> {
+
+}
