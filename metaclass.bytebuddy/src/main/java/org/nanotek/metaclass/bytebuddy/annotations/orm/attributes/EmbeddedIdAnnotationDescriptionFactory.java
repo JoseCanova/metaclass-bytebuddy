f5df4af4e39d20544096bb2663bd852ed564e28b
Jose Carlos Canova <jose.c.canova@gmail.com>
2025-05-14 11:56:50 -0300

    removed incorrect todo task.

--- metaclass.bytebuddy/src/main/java/org/nanotek/metaclass/bytebuddy/annotations/orm/attributes/EmbeddedIdAnnotationDescriptionFactory.java
+++ metaclass.bytebuddy/src/main/java/org/nanotek/metaclass/bytebuddy/annotations/orm/attributes/EmbeddedIdAnnotationDescriptionFactory.java
@@ -2,18 +2,17 @@ package org.nanotek.metaclass.bytebuddy.annotations.orm.attributes;
 
 import java.util.Optional;
 
-import org.nanotek.meta.model.rdbms.RdbmsMetaClassAttribute;
+import org.nanotek.meta.model.IdBase;
 import org.nanotek.metaclass.bytebuddy.annotations.AnnotationDescriptionFactory;
 
 import jakarta.persistence.EmbeddedId;
-import jakarta.persistence.Id;
 import net.bytebuddy.description.annotation.AnnotationDescription;
 
 /**
  * Produces jakarta.persistence.EmbeddedId using  single attribute type annotation.
  */
 public class EmbeddedIdAnnotationDescriptionFactory 
-implements AnnotationDescriptionFactory<EmbeddedId, RdbmsMetaClassAttribute> {
+implements AnnotationDescriptionFactory<EmbeddedId, IdBase<?,?>> {
 
 	private EmbeddedIdAnnotationDescriptionFactory() {
 	}
@@ -23,11 +22,10 @@ implements AnnotationDescriptionFactory<EmbeddedId, RdbmsMetaClassAttribute> {
 	}
 
 	@Override
-	//TODO: Change here, embeddedid can be a group of attributes.
-	public Optional<AnnotationDescription> buildAnnotationDescription(RdbmsMetaClassAttribute ma) {
+	public Optional<AnnotationDescription> buildAnnotationDescription(IdBase<?,?> ma) {
 		
 		return Optional.of(ma)
-				.filter(a -> a.isPartOfId())
+//				.filter(a -> a.isPartOfId())
 				.map(a -> AnnotationDescription.Builder
 						.ofType(EmbeddedId.class)
 						.build());
